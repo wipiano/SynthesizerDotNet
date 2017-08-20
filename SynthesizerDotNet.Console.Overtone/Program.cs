51e3372dfb585cb5e4160dfc0aabda1b9263fdb6
DESKTOP-3K4Q64N\Kohei <wipiano@gmail.com>
2017-08-21 01:51:34 +0900

    Add Factory Class

--- SynthesizerDotNet.Console.Overtone/Program.cs
+++ SynthesizerDotNet.Console.Overtone/Program.cs
@@ -10,17 +10,18 @@ namespace SynthesizerDotNet.Console.Overtone
         {
             const int sps = 44100;
             const double A4 = 440;
-            var calc = new EqualTemperamentFrequencyCalculator(A4);
+            var factory = new FactorySinWaveGenerator() {SamplesPerSec = sps};
 
-            var volumes = new[] {1.0, 0.5, 0.3, 0.4, 0.25, 0.35};
+            var volumes = new[] {1.0, 0.5, 0.8, 0.4, 0.25, 0.9};
 
             var generator = HarmonicsHelper
                 .GetHarmonics(A4, 6)
-                .Select((freq, i) => new SinWaveGenerator(sps, freq).Volume(volumes[i]))
+                .Select((freq, i) => factory.Create(freq).Volume(volumes[i]))
                 .Aggregate((first, second) => first.Combine(second))
-                .Normalize(sps);
+                .Normalize(sps)
+                .Volume(0.5);
 
-            var wave = generator.GetWave().Take(sps).ToArray();
+            var wave = generator.GetWave().Take(100000).ToArray();
 
             var wav = new WavData()
             {
---
+++ SynthesizerDotNet/WaveGenerators/FactorySinWaveGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace SynthesizerDotNet.WaveGenerators
+{
+    public class FactorySinWaveGenerator
+    {
+        public int SamplesPerSec { get; set; }
+
+        public FactorySinWaveGenerator()
+        {
+        }
+
+        public SinWaveGenerator Create(double freq)
+        {
+            return new SinWaveGenerator(this.SamplesPerSec, freq);
+        }
+    }
+}
